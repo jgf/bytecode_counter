be12f0a87717391ec7c9863913f6fbe171ba399c
Juergen Graf <juergen.graf@gmail.com>
2012-05-24 01:56:29 +0200

    testcase with "multiple" methods

--- Test.java
+++ Test.java
@@ -2,15 +2,19 @@ public class Test {
 
 public static void main(String argv[]) {
 	for (int i = 0; i < 100; i++) {
-		foo(3,4);
-		foo(-3,-4);
+		foo(3, 4);
+		foo(-3, -4);
 	}
 }
 
 public static void foo(int x, int y) {
 	if (x < 0 && y < 0) {
-		foo(y, x+1);
+		foo(y, add(x, 1));
 	}	
+}
+
+public static int add(int a, int b) {
+	return a + b;
 }
 
 }
